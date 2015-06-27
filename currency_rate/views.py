c3e5b5dd3a4ad7a3ebe55fe78b86e79909eb9ee8
Yura <even@mail.ru>
2015-06-27 22:05:47 +0400

    to beginning

--- currency_rate/views.py
+++ currency_rate/views.py
@@ -17,7 +17,7 @@ def conversion(rate_dict):
     return result
 #======================================================================================================================================================
 def conversion_no_obj(rate_dict):
-    print '1234567810'
+    print '123456789'
     success = True
     try:
         temp = float(rate_dict['amount'])
